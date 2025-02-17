68abb433248e1ad7b27a22861284c9bd0f63acaf
Nishant <nishant.singh@students.iiserpune.ac.in>
2025-02-17 17:12:40 +0530

    Implemented noise in CA3p

--- model_brian2/DG_CA3p/models.py
+++ model_brian2/DG_CA3p/models.py
@@ -16,15 +16,11 @@ CA3p_PR_params = {
     'Vk': -75.0*mV,
     'Vca': 80.0*mV,
     'Vl': -60.0*mV,
-    'Vnmda': 3.5*mV,
-    'Vdmpa': 0.0*mV,
-    'p': 0.5,
+    'p': 0.75,
     'Cm': 3.0*uF,
     'Isapp': 0.0,
     'Idapp': 0.0,
-    'Gmax_ampa':  1*msiemens,
-    'G_gabaA':  1*msiemens,
-    'G_gabaB':  3*msiemens
+    'noise': 1*uA,
 }
 
 eqnCA3_PR = '''
@@ -58,8 +54,10 @@ Iampa : amp
 Inmda : amp
 
 ## ODEs
-dVs/dt  = (- Ileaks - Ina - Ikdr - Ids/p)/Cm : volt
-dVd/dt  = (- Ileakd - Ica - Ikca - Ikahp - Iampa - Inmda - Isd/(1-p))/Cm : volt
+# dVs/dt  = (- Ileaks - Ina - Ikdr - Ids/p)/Cm : volt
+# dVd/dt  = (- Ileakd - Ica - Ikca - Ikahp - Iampa - Inmda - Isd/(1-p))/Cm : volt
+dVs/dt  = (- Ileaks - Ina - Ikdr - Ids/p + noise*sqrt(1/1*ms)*xi)/Cm : volt
+dVd/dt  = (- Ileakd - Ica - Ikca - Ikahp - Iampa - Inmda - Isd/(1-p) + noise*sqrt(1/1*ms)*xi_1)/Cm : volt
 dm/dt   = alphaM - m*(alphaM + betaM) : 1
 dn/dt   = alphaN - n*(alphaN + betaN) : 1
 dh/dt   = alphaH - h*(alphaH + betaH) : 1
@@ -72,14 +70,15 @@ dCa/dt  = (-0.13*Ica/uA - 0.075*Ca)/ms : 1
 #####################################################################
 ## MFB-CA3p synapse model
 MFB_CA3p_syn_params = {
-    'tau_AMPA': 2.0 * ms,  # AMPA receptor time constant
-    'g_AMPA': 0.3 * mS,      # Maximum AMPA conductance
+    'tau_AMPA': 2.0 * ms,      # AMPA receptor time constant
+    'g_AMPA': 0.3 * mS,        # Maximum AMPA conductance
     'V_AMPA': 0.0 * mV,        # AMPA reversal potential
+    'g_NMDA': 0.327 * mS,      # Maximum NMDA conductance
+    # 'g_NMDA': 0 * mS,      # Maximum NMDA conductance
     'V_NMDA': 0.0 * mV,        # NMDA reversal potential
-    'g_NMDA': 0.327 * mS,
     'tau_NMDA_rise': 2.0 * ms,
     'tau_NMDA_decay': 100.0 * ms,
-    'alpha': 0.5 / ms,
+    'alpha': 0.5 / ms,  ## default: 0.5 / ms
     'Mg2': 1.0
 }
 
